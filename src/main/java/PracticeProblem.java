21868bf3d825250b6e64351cc977dc98dd451a40
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-01 14:02:14 +0000

    a

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,14 +1,15 @@
 public class PracticeProblem {
 
-	public static int sum2D(int[][] arr) {
+	public static int sum2D(int[][] int2DArray) {
 		int sum = 0;
-		for (int i = 0; i < arr.length; ++i) {
-			for (int j = 0; j < arr[i].length; ++j) {
-				sum += arr[i][j];
+		for (int i = 0; i < int2DArray.length; i++) {
+			for (int j = 0; j < int2DArray[i].length; j++) {
+				sum += int2DArray[i][j];
 			}
 		}
 		return sum;
 	}
+
 	public static int sumRow(int[][] array, int rowNum) {
 		int sum = 0;
 		for(int column = 0; column < array[rowNum].length; column++) {
